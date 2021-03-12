2348d46886d4e1b112243500958bbbeb81a6b8c6
Ian Carey <icarey@kitmanlabs.com>
2021-03-12 17:06:36 +0000

    Added medium editor, bumped to v2.0

--- src/js/main.js
+++ src/js/main.js
@@ -1,64 +1,37 @@
-function setCursorToEnd(editor) {
-  var range = document.createRange();
-  var sel = window.getSelection();
-  range.setStart(editor, editor.childNodes.length);
-  range.collapse(true);
-  sel.removeAllRanges();
-  sel.addRange(range);
-}
-
-var messages = [
-  "Let's get to it!!",
-  "Your awesome ideas here...",
-  "Let ME remember that for you...",
-];
-
-function getRandomPlaceholderMessage() {
-  var index = Math.floor((Math.random() * (messages.length)));
-  return messages[index];
-}
-
-function setEditorContent(editor) {
-  var content = window.localStorage.getItem("notes");
-  if (content) {
-    editor.innerText = content + " ";
-  } else {
-    editor.innerText = getRandomPlaceholderMessage();
-  }
-}
-
 function saveContent() {
   var editor = document.getElementById("editor");
-  window.localStorage.setItem("notes", editor.innerText.trim());
-}
-
-function clearEditorIfNoSavedContent() {
-  var content = window.localStorage.getItem("notes");
-  var editor = document.getElementById("editor");
-  if (!content && messages.includes(editor.innerText)) {
-    var editor = document.getElementById("editor");
-    editor.innerText = "";
-    editor.focus();
-  }
-}
-
-function revertIfNoContent() {
-  var editor = document.getElementById("editor");
-  setEditorContent(editor);
+  window.localStorage.setItem("notes", editor.innerHTML);
 }
 
 function onPageLoad() {
-  var editor = document.getElementById("editor");
+  var editor = new MediumEditor('.editor',
+    {
+      delay: 500,
+      buttonLabels: 'fontawesome',
+      targetBlank: true,
+      toolbar: {
+          buttons: ['h1','h2','h3','bold', 'italic', 'quote', 'anchor', 'unorderedlist', 'orderedlist']
+      },
+      anchor: {
+          placeholderText: 'Enter Link'
+      },
+      paste: {
+          cleanPastedHTML: true
+      },
+      anchorPreview: {
+          hideDelay: 300
+      },
+      placeholder: {
+          text: 'Click to edit'
+      }
+    }
+  );
+  editor.subscribe('editableInput', function(event, editorElement) { saveContent(); });
+  var editorElement = document.getElementById("editor");
   var content = window.localStorage.getItem("notes");
-  editor.onfocus = clearEditorIfNoSavedContent;
-  editor.onblur = revertIfNoContent;
-  setEditorContent(editor);
-  if(content) {
-    setCursorToEnd(editor);
-    editor.focus();
-  }
+  editorElement.innerHTML = content
+  editorElement.focus();
 }
 
 window.onkeyup = saveContent
-window.onkeydown = clearEditorIfNoSavedContent
 window.onload = onPageLoad
